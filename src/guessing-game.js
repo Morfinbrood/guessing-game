6ec470b610af7592abf2904fcaed6a034166f7c7
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-23 18:47:34 +0300

    Guessing-game/fix: updated specification 'Readme.md' and deleted all attention comments about conflict

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -16,18 +16,13 @@ class GuessingGame {
         this.mid = Math.ceil((this.minimal + this.maximum) / 2);
         return this.mid;
 	}
-	
-	// ATTENTION!!! in spec says :
-		//  lower() "this method is called if prev call of guess() returned number which is lower than answer"
-		// greater() "this method is called if prev call of guess() returned number which is lower than answer"
-	// But it conflicts with these tests. And i implemented task with revert logic (lower() and greater()) who works with these tests.
 
-	// IMPORTANT! this method is called if prev call of guess() returned number which is greater than answer
+	//this method is called if prev call of guess() returned number which is greater than answer
     lower() {
         this.maximum = this.mid;
 	}
 	
-	// IMPORTANT! this method is called if prev call of guess() returned number which is lesser than answer
+	// this method is called if prev call of guess() returned number which is lesser than answer
     greater() {
         this.minimal = this.mid;
     }
